b8f52cfd6072b1943699ba6ccc2a6b3fd7ad7812
leoeberts <leonardo.eberts@gmail.com>
2017-08-06 01:05:51 -0300

    "Remove" button

---
+++ imports/ui/image.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+
+import { Images } from '../api/images.js';
+
+import './image.html';
+
+Template.imageThumb.events({
+    'click .delete'() {
+        Images.remove(this._id);
+    },
+});
+
