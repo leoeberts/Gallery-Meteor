8b9df7560bd33416c91eb427927d5c35b64bc96b
leoeberts <leonardo.eberts@gmail.com>
2017-08-06 23:58:01 -0300

    Client-side filtering with Reactive Dictionary; First version of data validation; and Minor changes in CSS.

--- imports/api/images.js
+++ imports/api/images.js
@@ -7,8 +7,16 @@ export const Images = new Mongo.Collection('images');
 
 Meteor.methods({
     'images.insert'(url, title, description) {
-        check(url, String);
+        var validURL = Match.Where(function(url) {
+            check(url, String);
+            var regexp = /(?:jpg|gif|png|jpeg)/g;
+            return regexp.test(url);
+        });
+        check(url, validURL);
+
         check(title, String);
+        check(true, title.length > 0);
+
         check(description, String);
 
         Images.insert({
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,23 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import {Images} from "../api/images.js";
 
 import './body.html';
 import './image.js';
 
-Template.body.helpers( {
-    images() {
-        return Images.find({});
-    }
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
 });
 
+Template.body.helpers( {
+    images() {
+        if (Template.instance().state.get('hideBrokenImages')) {
+            return Images.find({url: {$regex: "(?:jpg|gif|png|jpeg)"}});
+        }
+        return Images.find({});
+    },
+});
 
 Template.body.events({
     'submit .new-image'(event) {
@@ -23,4 +31,7 @@ Template.body.events({
         target.description.value = '';
         target.addModal.checked = false;
     },
+    'change .hide-broken-images input'(event, instance) {
+        instance.state.set('hideBrokenImages', event.target.checked);
+    }
 });
